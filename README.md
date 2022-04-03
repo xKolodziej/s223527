037d5e685a23a4e4211eebf5f64cb6fb2ae0dee0
s-116-21 <s-116-21@gumiforest.ae.krakow.pl>
2022-04-03 16:11:32 +0200

    22-04-03 MK

--- README.md
+++ README.md
@@ -4,17 +4,22 @@
 
 |Składowa|Selektor|Zmienna|
 |--------|--------|-------|
-|opinia|div.js_product-review|Review|
-|identyfikator_opinii|\[data-entry-id]|review_id|
-|autor|span.user-post__author-name|author|
-|rekomendacja|span.user-post__author-recomendation > em|recommenadtion|
-|liczba_gwiazdek|span.user-post__score-count|stars|
-|tresc|div.user-post__text|content|
-|data_wystawienia|div.user-post__published > time:nth-child(1)\[datetime\]|[ublish_date|
-|data_zakupu|div.user-post__published > time:nth-child(2)\[datetime\]|purchase_date|
-|dla_ilu_przydatna||useful|
-|dla_ilu_nieprzydatna||useless|
-|lista_zalet|review-feature__title--positives > review-feature__item|Positives_list|
-|lista_wad|review-feature__title--negatives > review-feature__item|Negatives_list|
+|opinia|div.js_product-review|Review|bs4.element.Tag|
+|identyfikator_opinii|\["data-entry-id"]|review_id|str|
+|autor|"span.user-post__author-name"|author|bs4.element.ResultSet|str|
+|rekomendacja|"span.user-post__author-recomendation"> em|recommenadtion||
+|liczba_gwiazdek|"span.user-post__score-count"|stars|float|
+|tresc|"div.user-post__text"|content|str|
+|data_wystawienia|div.user-post__published > time:nth-child(1)\[datetime\]|[publish_date|str|
+|data_zakupu|span.user-post__published > time:nth-child(2)\[datetime\]|purchase_date|str|
+|dla_ilu_przydatna|span[id^=votes-yes]|useful|int|
+|dla_ilu_nieprzydatna|span[id^=votes-no]|useless|int|
+|lista_zalet|div.review-feature__title--positives > div.review-feature__item|Positives_list|str|
+|lista_wad|div.review-feature__title--negatives > div.review-feature__item|Negatives_list|str|
 ||||
 ||||
+## Etapy pracy nad projektem
+- pobranie składowych pojedynczej opinii do niezaleznych zmiennych
+- zapisanie składowych do pojedynczej opiinii do obiektu słownika (disctionary)
+- pobranie wszystkich opinii z pojedynczej strony i zapisanie ich do listy słowników
+- pobranie opinii o wszkazym produkcie i zapisanie ich do pliku
--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 
 url = "https://www.ceneo.pl/95365253#tab=reviews"
@@ -6,4 +7,57 @@ response = requests.get(url)
 
 page_dom = BeautifulSoup(response.text, 'html.parser')
 
-print(page_dom.prettify())
+reviews = page_dom.select("div.js_product-review")
+
+all_reviews = []
+
+for review in reviews:
+    review = reviews.pop(6)
+
+    review_id = review["data-entry-id"]
+
+    author= review.select("span.user-post__author-name").pop(0).text.strip()
+
+    recommenadtion = review.select("span.user-post__author-recomendation").pop(0).text
+
+    recommenadtion = True if recommenadtion == "Polecam" else False if recommenadtion == "Nie Polecam" else None
+
+    stars = review.select("span.user-post__score-count").pop(0).text
+    stars = float(stars.split("/").pop(0).replace(",","."))
+
+    content = review.select("div.user-post__text").pop(0).text
+
+    content = content.replace("\n", " ")
+
+    publish_date = review.select("span.user-post__published > time:nth-child(1)").pop(0)["datetime"]
+
+    purchase_date = review.select("span.user-post__published > time:nth-child(2)").pop(0)["datetime"]
+
+    useful = review.select("span[id^=votes-yes]").pop(0).text
+    useful=int(useful)
+
+    useless = review.select("span[id^=votes-no]").pop(0).text
+    useless=int(useless)
+
+    Positives_list= review.select("div.review-feature__title--positives ~ div.review-feature__item")
+
+
+    Negatives_list = review.select("div.review-feature__title--negatives ~ div.review-feature__item")
+
+    single_review = {
+        "review_id": review_id,
+        "author": author,
+        "reccomendation": recommenadtion,
+        "stars": stars,
+        "content": content,
+        "publish_date": publish_date,
+        "purchase_date": purchase_date,
+        "useful": useful,
+        "useless": useless,
+        "Positives_list": Positives_list,
+        "Negatives_list": Negatives_list
+
+    }
+    all_reviews.append(single_review)
+    print(json.dumps(single_review, indent=4, ensure_ascii=False))
+
