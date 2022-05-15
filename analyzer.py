e1fe4095cb1e1c0d45b0688830fb9d8c66edebd3
xKolodziej <92814280+xKolodziej@users.noreply.github.com>
2022-05-15 15:55:23 +0200

    Update

--- analyzer.py
+++ analyzer.py
@@ -3,3 +3,13 @@ import pandas as pd
 
 print("[filename.split(". ")[0] for filename in os.listdir("./revievs")], sep="\n")
 product_id = input("Podaj identyfikator produktu")
+      
+opinions = pd.read_json("reviews/"+product_id+".json")
+      
+opinions_count = len(opinions)
+pros_count = opinions.pros["pros"].astype(bool).sum()
+cons_count = opinions.pros["pros"].astype(bool).sum()  
+average_score = opinions["stars"].mean().round(2)
+      
+recommendations = opinions["recommendation"].
+print(pros_count)
