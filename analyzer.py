56df4b89efebafb65285f9fcc21a968a1a348a4f
xKolodziej <92814280+xKolodziej@users.noreply.github.com>
2022-05-15 15:58:07 +0200

    Update

--- analyzer.py
+++ analyzer.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+from matplotlib import pyplot as plt
 
 print("[filename.split(". ")[0] for filename in os.listdir("./revievs")], sep="\n")
 product_id = input("Podaj identyfikator produktu")
@@ -11,5 +12,6 @@ pros_count = opinions.pros["pros"].astype(bool).sum()
 cons_count = opinions.pros["pros"].astype(bool).sum()  
 average_score = opinions["stars"].mean().round(2)
       
-recommendations = opinions["recommendation"].
+recommendations = opinions["recommendation"].valu_counts()
+recommedations.plot.pie()
 print(pros_count)
